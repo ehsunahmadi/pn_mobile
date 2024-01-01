70f689091c6fc2ef1bd198df388cdb22c264cc5c
ehsun ahmadi <ehsun.ahmandi@gmail.com>
2024-01-01 13:01:17 +0330

    minor redirect logic handled

--- app/registration/index.tsx
+++ app/registration/index.tsx
@@ -1,5 +1,13 @@
 import { Redirect } from 'expo-router';
+import { useSnapshot } from 'valtio';
+import { state } from '../../store';
 
 export default function Index() {
+  const { authStatus } = useSnapshot(state);
+
+  if (authStatus === 'authenticated') {
+    return <Redirect href={'/'} />;
+  }
+
   return <Redirect href={'/registration/phone_number'} />;
 }
